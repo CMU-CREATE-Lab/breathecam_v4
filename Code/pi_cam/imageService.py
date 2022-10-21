5e11a5fbe083ae359f783d47f71e63794bde4c05
Randy Sargent <randy.sargent@gmail.com>
2022-10-21 08:54:32 -0400

    pi_cam: Record metadata in exif
    Records all picamera2 image metadata, including color gains,
    into MakerNote.
    
    This tag can be read from the commandline like so:
    exiftool -b  image.jpg  -MakerNoteUnknownText

--- Code/pi_cam/imageService.py
+++ Code/pi_cam/imageService.py
@@ -12,6 +12,8 @@ from picamera2 import Picamera2
 import libcamera
 from os.path import exists
 import ArducamMux
+import piexif
+import json
 
 class ImageService:
     def __init__(self, config: ServiceConfig, test_only=False):
@@ -32,28 +34,77 @@ class ImageService:
         wot = disk_usage(self.config.base_dir())
         return wot.used / wot.total
 
+    # Code modified from Helpers.save
+    def save_image(self, img, metadata, file_output, format=None):
+        start_time = time.monotonic()
+        exif = b''
+        if isinstance(format, str):
+            format_str = format.lower()
+        elif isinstance(file_output, str):
+            format_str = file_output.split('.')[-1].lower()
+        else:
+            raise RuntimeError("Cannot detemine format to save")
+        if format_str in ('jpg', 'jpeg'):
+            if img.mode == "RGBA":
+                # Nasty hack. Qt doesn't understand RGBX so we have to use RGBA. But saving a JPEG
+                # doesn't like RGBA to we have to bodge that to RGBX.
+                img.mode = "RGBX"
+            # Make up some extra EXIF data.
+            zero_ifd = {piexif.ImageIFD.Make: "Raspberry Pi",
+                        piexif.ImageIFD.Model: self.picam2.camera.id,
+                        piexif.ImageIFD.Software: "Picamera2",
+                        piexif.ImageIFD.MakerNoteSafety: 1}
+            total_gain = metadata["AnalogueGain"] * metadata["DigitalGain"]
+            
+            exif_ifd = {piexif.ExifIFD.ExposureTime: (metadata["ExposureTime"], 1000000),
+                        piexif.ExifIFD.ISOSpeedRatings: int(total_gain * 100),
+                        piexif.ExifIFD.MakerNote: f"Picamera2 {json.dumps(metadata)}".encode("utf8")}
+            exif = piexif.dump({"0th": zero_ifd, "Exif": exif_ifd})
+        # compress_level=1 saves pngs much faster, and still gets most of the compression.
+        png_compress_level = self.picam2.options.get("compress_level", 1)
+        jpeg_quality = self.picam2.options.get("quality", 90)
+        img.save(file_output, compress_level=png_compress_level, quality=jpeg_quality, exif=exif, format=format)
+        end_time = time.monotonic()
+        self.picam2.log.info(f"Saved {self} to file {file_output}.")
+        self.picam2.log.info(f"Time taken for encode: {(end_time-start_time)*1000} ms.")
+
+    def capture_file_and_metadata(self, file_output, format=None):
+        # Wait for next capture and get its Request, with metadata
+        request = self.picam2.capture_request()
+        metadata = request.get_metadata() # Copies data from request
+
+        # Create PIL image from request
+        img = request.make_image("main") # Copies data from request
+
+        # We've copied everything we need from request, so release it (for the pipeline to proceed?)
+        request.release()
+
+        self.save_image(img, metadata, file_output, format)
+
+        return metadata
+
     def grabLoop(self):
         self.log.info(f"Instantiating Picamera2 with tuning file {self.tuning_file}")
-        picam2 = Picamera2(tuning=self.tuning_file)
+        self.picam2 = Picamera2(tuning=self.tuning_file)
         transform = libcamera.Transform(rotation=self.rotation[0])
 
         # preview defaults to lower resolution, auto-exposure and auto-white-balance
-        preview_config = picam2.create_preview_configuration()
+        preview_config = self.picam2.create_preview_configuration()
         preview_config["transform"] = transform
-        picam2.configure(preview_config)
+        self.picam2.configure(preview_config)
 
-        picam2.start()
+        self.picam2.start()
         self.log.info("Running for 2 seconds in preview move to lock auto exposure")
         time.sleep(2)
-        picam2.stop()
+        self.picam2.stop()
 
         self.log.info("Reconfiguring camera for still")
         # still defaults to full-resolution, auto-exposure and auto-white-balance
-        still_config = picam2.create_still_configuration()
+        still_config = self.picam2.create_still_configuration()
         still_config["transform"] = transform
-        picam2.configure(still_config)
+        self.picam2.configure(still_config)
 
-        picam2.start()
+        self.picam2.start()
         logging.getLogger('picamera2').setLevel(logging.WARNING)
 
         image_dir = self.config.image_dir()
@@ -98,7 +149,8 @@ class ImageService:
 
             before = time.time()
 
-            picam2.capture_file(tmp_filename)
+            #picam2.capture_file(tmp_filename)
+            md = self.capture_file_and_metadata(tmp_filename)
             if self.test_only:
                 os.unlink(tmp_filename)
                 sys.exit(0)
@@ -109,7 +161,6 @@ class ImageService:
             os.link(current_filename, upload_filename)
             open(f"{image_dir}/last_capture.timestamp","w").write("\n")
             
-            md = picam2.capture_metadata()
             sensor_time_epoch = md['SensorTimestamp']/1e9 - time.clock_gettime(time.CLOCK_BOOTTIME) + time.time()
             sensor_time_fmt = datetime.datetime.fromtimestamp(sensor_time_epoch).strftime('%H:%M:%S.%f')[:-3]
             self.log.info(f"{upload_filename} capture (exp {md['ExposureTime']/1000}ms, sensortime {sensor_time_fmt} took {(after - before) * 1000:.0f}ms")
