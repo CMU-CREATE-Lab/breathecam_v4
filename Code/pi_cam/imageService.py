cde8e2d192c97e615be5d1e0ee8072c94bc349ad
Randy Sargent <randy.sargent@gmail.com>
2022-12-13 22:54:57 -0500

    Discard corrupt images (experimental)

--- Code/pi_cam/imageService.py
+++ Code/pi_cam/imageService.py
@@ -1,3 +1,6 @@
+import codecs
+import threading
+import numpy as np
 import datetime
 import math
 import shutil
@@ -8,12 +11,14 @@ import time
 import logging
 from serviceConfig import ServiceConfig
 import os
-from picamera2 import Picamera2
+from picamera2 import Picamera2, CompletedRequest
+from picamera2.request import _MappedBuffer
 import libcamera
 from os.path import exists
 import ArducamMux
 import piexif
 import json
+import ctypes
 
 class ImageService:
     def __init__(self, config: ServiceConfig, test_only=False):
@@ -63,25 +68,51 @@ class ImageService:
         # compress_level=1 saves pngs much faster, and still gets most of the compression.
         png_compress_level = self.picam2.options.get("compress_level", 1)
         jpeg_quality = self.picam2.options.get("quality", 90)
-        img.save(file_output, compress_level=png_compress_level, quality=jpeg_quality, exif=exif, format=format)
+        img.save(file_output, compress_level=png_compress_level, quality=jpeg_quality, exif=exif)
         end_time = time.monotonic()
-        self.log.info(f"Saved {self} to file {file_output}.")
+        self.log.info(f"Saved image to file {file_output}.")
         self.log.info(f"Time taken for encode: {(end_time-start_time)*1000} ms.")
 
-    def capture_file_and_metadata(self, file_output, format=None):
-        # Wait for next capture and get its Request, with metadata
-        request = self.picam2.capture_request()
+    def save_file_and_metadata(self, request: CompletedRequest, capture_timestamp: int):
+        image_dir = self.config.image_dir()
+        current_dir = f"{image_dir}/current"
+        os.makedirs(current_dir, exist_ok=True)
+
         metadata = request.get_metadata() # Copies data from request
 
         # Create PIL image from request
-        img = request.make_image("main") # Copies data from request
+        img = request.make_image("main") # Referenced data from request;  do not release request until after done with image
 
-        # We've copied everything we need from request, so release it (for the pipeline to proceed?)
+        tmp_filename = f"{current_dir}/{capture_timestamp:.0f}-{os.getpid()}-tmp.jpg"
+
+        # Current image
+        current_filename = f"{current_dir}/current.jpg"
+        current_tmp_filename = f"{current_dir}/current-tmp{os.getpid()}-{threading.get_ident()}.jpg"
+        # File for upload
+        upload_filename = f"{image_dir}/{capture_timestamp:.0f}.jpg"
+
+        before = time.monotonic()
+        self.save_image(img, metadata, current_tmp_filename, format)
+
+        if self.test_only:
+            os.unlink(tmp_filename)
+            sys.exit(0)
+
+        # Atomically replace current.jpg, for interactive display (e.g. focusing)
+        os.rename(current_tmp_filename, current_filename)
+
+        # Hard-link to image in "to be uploaded" directory
+        os.link(current_filename, upload_filename)
+        open(f"{image_dir}/last_capture.timestamp","w").write("\n")
+        
+        sensor_time_epoch = metadata['SensorTimestamp']/1e9 - time.clock_gettime(time.CLOCK_BOOTTIME) + time.time()
+        sensor_time_fmt = datetime.datetime.fromtimestamp(sensor_time_epoch).strftime('%H:%M:%S.%f')[:-3]
+        after = time.monotonic()
+        self.log.info(f"Saving {upload_filename} (exp {metadata['ExposureTime']/1000}ms, sensortime {sensor_time_fmt} took {(after - before) * 1000:.0f}ms")
+        self.log.info(f"  metadata: {metadata}")
+
+        # We've copied everything we need from request, so release it for the pipeline to proceed
         request.release()
-
-        self.save_image(img, metadata, file_output, format)
-
-        return metadata
 
     def grabLoop(self):
         self.log.info(f"Instantiating Picamera2 with tuning file {self.tuning_file}")
@@ -100,88 +131,50 @@ class ImageService:
 
         self.log.info("Reconfiguring camera for still")
         # still defaults to full-resolution, auto-exposure and auto-white-balance
-        still_config = self.picam2.create_still_configuration()
+        still_config = self.picam2.create_still_configuration(raw={}, buffer_count=1)
         still_config["transform"] = transform
+        self.log.info(f"Initial config is {still_config}")
+        self.picam2.align_configuration(still_config)
+        self.log.info(f"Config after alignment is {still_config}")
         self.picam2.configure(still_config)
 
         self.picam2.start()
         logging.getLogger('picamera2').setLevel(logging.WARNING)
 
-        image_dir = self.config.image_dir()
-        current_dir = f"{image_dir}/current"
-        os.makedirs(current_dir, exist_ok=True)
         interval = self.config.interval()
         last_capture_time = 0
+        corrupt_count = 0
+        all_count = 0
 
+        sentinel_val = b"\xff\x00\x11\xaa\xde\xad\xbe\xef"
         while True:
-            usage = self.checkDiskUsage()
-            if usage > 0.95:
-                self.log.error(f"Refusing to capture image as disk is too full ({usage*100:.1f}%")
-                continue
-            current_time = time.time()
-            slop = 0.01 # seconds
-            next_capture_time = max(0, math.floor((current_time + interval - slop) / interval) * interval)
-            if last_capture_time:
-                desired_capture_time = math.floor((last_capture_time + interval * 1.5) / interval) * interval
-            
-                if desired_capture_time >= next_capture_time:
-                    next_capture_time = desired_capture_time
+            # Capture next frame
+            before = time.monotonic()
+            request: CompletedRequest = self.picam2.capture_request()
+            #request.
+            capture_duration = time.monotonic() - before
+            self.log.info(f"Captured frame in {capture_duration*1000:.1f}ms")
+            all_count += 1
+            with _MappedBuffer(request, "raw") as b:
+                self.log.info(f"len={len(b)}, sentinel area={b[-36:-28].hex(' ')}")
+                save_good_bad = False
+                if b[-36:-28] == sentinel_val:
+                    self.log.info("CORRUPT IMAGE!  Skipping.")
+                    if save_good_bad:
+                        request.save("main", datetime.datetime.now().strftime("test/bad.%H%M%S.%f.jpg"))
+                    request.release()
                 else:
-                    self.log.warning(f"MISSED {(next_capture_time - desired_capture_time) / interval:.0f} CAPTURES")
-
-            sleep_duration = next_capture_time - current_time
-
-            #time.localtime(next_capture_time).isoformat()
-            
-            next_capture_time_fmt = (
-                datetime.datetime.fromtimestamp(next_capture_time).strftime('%H:%M:%S'))
-
-            #     '%H:%M:%S.'
-            #     time.localtime(next_capture_time)
-            self.log.info(f"Sleeping {sleep_duration * 1000:.0f}ms until {next_capture_time_fmt}")
-            time.sleep(sleep_duration);
-
-            tmp_filename = f"{current_dir}/{next_capture_time:.0f}-{os.getpid()}-tmp.jpg"
-            # Current image
-            current_filename = f"{current_dir}/current.jpg"
-            # File for upload
-            upload_filename = f"{image_dir}/{next_capture_time:.0f}.jpg"
-
-            before = time.time()
-
-            #picam2.capture_file(tmp_filename)
-            md = self.capture_file_and_metadata(tmp_filename)
-            if self.test_only:
-                os.unlink(tmp_filename)
-                sys.exit(0)
-            after = time.time()
-            # Atomically replace current.jpg, for interactive display (e.g. focusing)
-            os.rename(tmp_filename, current_filename)
-            # Hard-link to image in "to be uploaded" directory
-            os.link(current_filename, upload_filename)
-            open(f"{image_dir}/last_capture.timestamp","w").write("\n")
-            
-            sensor_time_epoch = md['SensorTimestamp']/1e9 - time.clock_gettime(time.CLOCK_BOOTTIME) + time.time()
-            sensor_time_fmt = datetime.datetime.fromtimestamp(sensor_time_epoch).strftime('%H:%M:%S.%f')[:-3]
-            self.log.info(f"{upload_filename} capture (exp {md['ExposureTime']/1000}ms, sensortime {sensor_time_fmt} took {(after - before) * 1000:.0f}ms")
-
-            self.log.info(f"  metadata: {md}")
-
-        while True:
-            startTime = time.time()
-            if startTime > (self.last_grab + self.config.interval()):
-                usage = self.checkDiskUsage()
-                if usage < 0.9 :
-                    self.grabMulti()
-                    self.last_grab = startTime
-                    endTime = time.time()
-                    self.log.info("Grab took %.2f seconds", endTime - startTime);
-                else:
-                    self.log.warning("Disk Usage %d%%, not grabbing new image",
-                                     int(usage * 100));
-                    time.sleep(60)
-            else:
-                time.sleep(0.1)
+                    b[-36:-28] = sentinel_val
+                    if save_good_bad:
+                        request.save("main", datetime.datetime.now().strftime("test/good.%H%M%S.%f.jpg"))
+                    current_time = time.time()
+                    # Round down to beginning of current period
+                    this_capture_time = math.floor(current_time / interval) * interval
+                    if this_capture_time > last_capture_time:
+                        last_capture_time = this_capture_time
+                        self.save_file_and_metadata(request, this_capture_time)
+                    else:
+                        request.release()
 
     def run(self):
         self.grabLoop()
